745da5e30b2bf8461c2a8e38e1658f48a08b8980
irfanpra <irfanpra@gmail.com>
2017-01-13 23:41:11 +0100

    afg

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-$konekcija = new PDO("mysql:dbname=baza;host=localhost", getenv("MYSQL_USERNAME"), getenv("PASSWORD"));
+$konekcija = new PDO("mysql:dbname=baza;host=mysql-57-centos7", getenv("MYSQL_USERNAME"), getenv("PASSWORD"));
 $rez = $konekcija->query("SELECT * FROM imena");
 foreach($rez as $red){
 	echo $red['ime']."<br>";
 }
 ?>
+
+mysql -u korisnik -p sifra < baza/baza.sql
