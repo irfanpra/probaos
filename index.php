d0479d132ff802ec68135c9c2c3644048fbaf1d5
irfanpra <irfanpra@gmail.com>
2017-01-13 23:41:53 +0100

    added action hooks

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$konekcija = new PDO("mysql:dbname=baza;host=mysql-57-centos7", getenv("MYSQL_USERNAME"), getenv("PASSWORD"));
+$konekcija = new PDO("mysql:dbname=baza;host=mysql-57-centos7", getenv("MYSQL_USERNAME"), getenv("MYSQL_PASSWORD"));
 $rez = $konekcija->query("SELECT * FROM imena");
 foreach($rez as $red){
 	echo $red['ime']."<br>";
