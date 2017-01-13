1668b3d4bbf133bfcdc8a173e01a12fb4e628532
irfanpra <irfanpra@gmail.com>
2017-01-13 21:40:07 +0100

    env

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$konekcija = new PDO("mysql:dbname=baza;host=localhost", "root", "");
+$konekcija = new PDO("mysql:dbname=baza;host=localhost", getenv("MYSQL_USERNAME"), getenv("PASSWORD"));
 $rez = $konekcija->query("SELECT * FROM imena");
 foreach($rez as $red){
 	echo $red['ime']."<br>";
